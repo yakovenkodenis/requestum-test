189ce37af77f2ae376531bae6763502ab3fda3d2
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2021-01-07 16:23:28 +0200

    Implement Searching functionality

--- src/components/search/History.tsx
+++ src/components/search/History.tsx
@@ -3,19 +3,25 @@ import { HistoryItem } from './HistoryItem';
 
 import { ReactComponent as RepoSearchIcon } from '../../icons/repo-search-icon.svg';
 import { ReactComponent as SearchIcon } from '../../icons/search.svg';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../core/redux/configureStore';
 
 interface IProps {
   searchTerm: string;
-  maxLength?: number;
+  onSelect: (value: string) => void;
 }
 
-export const History: FC<IProps> = ({ searchTerm, maxLength = 5 }) => {
-  const historyItems = [{ value: 'text' }];
+export const History: FC<IProps> = ({ searchTerm, onSelect }) => {
+  const historyItems = useSelector(
+    (state: RootState) => state.search.historyItems
+  );
 
-  const hitsoryItemElements = historyItems.map((item) => (
+  const hitsoryItemElements = historyItems.map((item, index) => (
     <HistoryItem
-      value={item.value}
+      key={index}
+      value={item}
       icon={<RepoSearchIcon className="repo-search-icon" />}
+      onSelectItem={onSelect}
     />
   ));
 
@@ -26,9 +32,10 @@ export const History: FC<IProps> = ({ searchTerm, maxLength = 5 }) => {
           <HistoryItem
             value={searchTerm}
             icon={<SearchIcon className="search-icon" />}
+            onSelectItem={onSelect}
           />
         )}
-        {hitsoryItemElements}
+        {hitsoryItemElements.length > 0 && hitsoryItemElements}
       </ul>
     </div>
   );
--- src/components/search/SearchField.tsx
+++ src/components/search/SearchField.tsx
@@ -1,35 +1,45 @@
-import { ChangeEvent, FC } from 'react';
+import { ChangeEvent, forwardRef, MouseEvent } from 'react';
 
 import searchKeyIcon from '../../icons/search-key-slash.svg';
+import { ReactComponent as DismissIcon } from '../../icons/dismiss.svg';
 
 interface IProps {
   searchTerm: string;
   searchCriteria?: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  clear: () => void;
 }
 
-export const SearchField: FC<IProps> = ({
-  searchTerm,
-  searchCriteria,
-  onChange,
-}) => {
-  const placeholder = searchCriteria
-    ? `Search ${searchCriteria.toLowerCase()}...`
-    : 'Search or jump to...';
+export const SearchField = forwardRef<HTMLInputElement, IProps>(
+  ({ searchTerm, onChange, clear }, ref) => {
+    const placeholder = 'Search or jump to...';
 
-  return (
-    <div className="search-field">
-      <input
-        type="text"
-        placeholder={placeholder}
-        value={searchTerm}
-        onChange={onChange}
-      />
-      <img
-        src={searchKeyIcon}
-        alt="Search key icon"
-        className="search-key-slash"
-      />
-    </div>
-  );
-};
+    const clearSearchField = (
+      e: MouseEvent<SVGSVGElement, globalThis.MouseEvent>
+    ) => {
+      e.preventDefault();
+      clear();
+    };
+
+    return (
+      <div className="search-field">
+        <input
+          ref={ref}
+          type="text"
+          placeholder={placeholder}
+          value={searchTerm}
+          onChange={onChange}
+        />
+        <img
+          src={searchKeyIcon}
+          alt="Search key icon"
+          className="search-key-slash"
+        />
+        <DismissIcon
+          className="clear-search-icon"
+          onMouseDown={clearSearchField}
+        />
+      </div>
+    );
+  }
+);
