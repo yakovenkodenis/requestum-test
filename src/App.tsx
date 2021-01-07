9e4e18afe8f0a5fadbb8b033810563588cffe093
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2021-01-07 16:26:01 +0200

    Add wrapper div for footer positioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import { FC } from 'react';
+
 import { Footer } from './components/footer';
 import { Header } from './components/header';
 import { ReposList } from './components/repos';
 
-function App() {
+const App: FC = () => {
   return (
-    <>
+    <div className="page-container">
       <Header />
       <ReposList />
       <Footer />
-    </>
+    </div>
   );
-}
+};
 
 export default App;
