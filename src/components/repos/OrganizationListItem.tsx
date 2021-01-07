1cf8d145aca64f9be9145bef4741ee96f1a4a5d1
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2021-01-07 14:57:27 +0200

    Set up markup and basic rendering logic for lists

---
+++ src/components/repos/OrganizationListItem.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react';
+
+import { Organization } from '../../core/entities/dataEntities';
+import { truncate } from '../../core/utils/truncate';
+
+interface IProps {
+  login: Organization['login'];
+  name: Organization['name'];
+  description: Organization['description'];
+  html_url: Organization['html_url'];
+  avatar_url: Organization['avatar_url'];
+}
+
+export const OrganizationListItem: FC<IProps> = ({
+  name,
+  login,
+  description,
+  html_url,
+  avatar_url,
+}) => {
+  const truncatedDescription = truncate(description ?? '', 350);
+  const displayName = name || login;
+
+  return (
+    <li className="organization-list-item">
+      <div className="repo-icon">
+        <img
+          src={avatar_url}
+          className="organization-avatar"
+          alt="Organization avatar"
+        />
+      </div>
+      <div className="organization-info">
+        <div className="repo-name">
+          <a
+            href={html_url}
+            className="repo-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="organization-name">{displayName}</span>
+          </a>
+        </div>
+        <p className="repo-description">{truncatedDescription}</p>
+      </div>
+    </li>
+  );
+};
--- src/components/repos/ReposList.tsx
+++ src/components/repos/ReposList.tsx
@@ -1,29 +1,83 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../core/redux/configureStore';
+import { commaNumberFormatter } from '../../core/utils/formatters';
 import { Pagination } from '../pagination';
 import { SkeletonRepo } from '../skeleton/SkeletonRepo';
+import { Spinner } from '../spinner';
+import { OrganizationListItem } from './OrganizationListItem';
 
 import { RepoListItem } from './RepoListItem';
 import './styles.scss';
 
+const SkeletonList: FC = () => (
+  <>
+    <SkeletonRepo />
+    <SkeletonRepo />
+    <SkeletonRepo />
+    <SkeletonRepo />
+    <SkeletonRepo />
+    <SkeletonRepo />
+    <SkeletonRepo />
+  </>
+);
+
+const ListComponentsMap = {
+  repository: RepoListItem,
+  organization: OrganizationListItem,
+};
+
 export const ReposList: FC = () => {
+  const isPending = useSelector((state: RootState) => state.ui.pending);
+  const data = useSelector((state: RootState) => state.data);
+
+  let items = null;
+  let totalResultsCount = null;
+
+  if (data && data.items) {
+    items = data.items.map((item) =>
+      React.createElement(ListComponentsMap[data.type] as FC, {
+        key: item.id,
+        ...item,
+      })
+    );
+
+    totalResultsCount = `${commaNumberFormatter(data.total_count)} ${
+      data.total_count === 1 ? 'result' : 'results'
+    }`;
+  }
+
+  // if (data && data.type === 'repository') {
+  //   items = data.items.map((item) => (
+  //     <RepoListItem key={item.id} repository={item} />
+  //   ));
+  // }
+
+  // if (data && data.type === 'organization') {
+  //   items = data.items.forEach((item) => {
+  //     console.log(item);
+  //   });
+  // }
+
   return (
     <main className="container">
-      <h3 className="search-results-title">4,976,514 repository results</h3>
+      {isPending ? (
+        <Spinner />
+      ) : (
+        data && <h3 className="search-results-title">{totalResultsCount}</h3>
+      )}
 
       <ul className="repos-list">
-        <SkeletonRepo />
-        <SkeletonRepo />
-        <SkeletonRepo />
-        <SkeletonRepo />
-
-        <RepoListItem />
-        <RepoListItem />
-        <RepoListItem />
-        <RepoListItem />
-        <RepoListItem />
+        {isPending ? (
+          <SkeletonList />
+        ) : items && items.length > 0 ? (
+          <>{items}</>
+        ) : (
+          <div>Let's search...</div>
+        )}
       </ul>
 
-      <Pagination />
+      <Pagination length={data.meta.lastPage} />
     </main>
   );
 };
